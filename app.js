027fad695738417e3875f4c4a53365087ab20a41
raphael.monteiro <raphahenrique2808@gmail.com>
2024-05-10 15:00:21 -0300

    Atualizações

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-alert('Boas vindas ao Jogo do Número Secreto!');
-let numeroSecreto = 29;
-console.log(numeroSecreto)
-let nomeUsuario = prompt('Coloque seu nome: ');
-let chute = parseInt(prompt('Escolha um número entre 1 e 30'));
+// alert('Boas vindas ao Jogo do Número Secreto!');
+// let numeroSecreto = 29;
+// console.log(numeroSecreto);
+// let nomeUsuario = prompt('Coloque seu nome: ');
+// let chute;
+// let tentativas = 1;
 
-if (chute == numeroSecreto ) {
-    alert(`Isso aí, ${nomeUsuario}! Você descobriu o número secreto: ${numeroSecreto}`);
-}  else {
-       if (chute > numeroSecreto) {
-        alert(`${nomeUsuario}, o número secreto é menor que: ${chute}`);
-       } else {
-        alert(`${nomeUsuario}, o número secreto é maior que: ${chute}`);
-       }
-}
+// // Enquanto chute não for igual ao número secreto
+// while (chute != numeroSecreto) {
+//     chute = parseInt(prompt('Escolha um número entre 1 e 30'));
+    
+// //Se chute for igual ao número secreto
+//     if (chute == numeroSecreto ) {
+//         alert(`Isso aí, ${nomeUsuario}! Você descobriu o número secreto: (${numeroSecreto}). Usou ${tentativas} tentativas para conseguir.`);
+//     }  else {
+//         if (chute > numeroSecreto) {
+//                 alert(`${nomeUsuario}, o número secreto é menor que: ${chute}`);
+//         } else {
+//                 alert(`${nomeUsuario}, o número secreto é maior que: ${chute}`);
+//         } 
+//         tentativas++; 
+//     }     
+// }
+
+
+
