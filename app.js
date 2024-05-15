b86ec255118b61f2f44816486ae0d42127394f40
raphael.monteiro <raphahenrique2808@gmail.com>
2024-05-15 15:04:07 -0300

    Atualizações - Jogo do Número Secreto

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-// alert('Boas vindas ao Jogo do Número Secreto!');
-// let numeroSecreto = 29;
-// console.log(numeroSecreto);
-// let nomeUsuario = prompt('Coloque seu nome: ');
-// let chute;
-// let tentativas = 1;
+alert('Boas vindas ao Jogo do Número Secreto!');
+let numeroSecreto = parseInt(Math.random()* 100 + 1);
+console.log(numeroSecreto);
+let nomeUsuario = prompt('Coloque seu nome: ');
+let chute;
+let tentativas = 1;
 
-// // Enquanto chute não for igual ao número secreto
-// while (chute != numeroSecreto) {
-//     chute = parseInt(prompt('Escolha um número entre 1 e 30'));
+// Enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = parseInt(prompt('Escolha um número entre 1 e 100'));
     
-// //Se chute for igual ao número secreto
-//     if (chute == numeroSecreto ) {
-//         alert(`Isso aí, ${nomeUsuario}! Você descobriu o número secreto: (${numeroSecreto}). Usou ${tentativas} tentativas para conseguir.`);
-//     }  else {
-//         if (chute > numeroSecreto) {
-//                 alert(`${nomeUsuario}, o número secreto é menor que: ${chute}`);
-//         } else {
-//                 alert(`${nomeUsuario}, o número secreto é maior que: ${chute}`);
-//         } 
-//         tentativas++; 
-//     }     
+//Se chute for igual ao número secreto
+    if (chute == numeroSecreto ) {
+        break;
+    }  else {
+        if (chute > numeroSecreto) {
+                alert(`${nomeUsuario}, o número secreto é menor que: ${chute}`);
+        } else {
+                alert(`${nomeUsuario}, o número secreto é maior que: ${chute}`);
+        } 
+        tentativas++; 
+    }     
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí, ${nomeUsuario}! Você descobriu o número secreto: (${numeroSecreto}). Usou ${tentativas} ${palavraTentativa} para conseguir.`);
+
+
+// if (tentativas > 1) {
+//     alert(`Isso aí, ${nomeUsuario}! Você descobriu o número secreto: (${numeroSecreto}). Usou ${tentativas} tentativas para conseguir.`);
+// } else { 
+//     alert(`Isso aí, ${nomeUsuario}! Você descobriu o número secreto: (${numeroSecreto}). Usou ${tentativas} tentativa para conseguir.`);
 // }
-
-
-
